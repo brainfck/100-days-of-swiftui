434575034003ee0bedafe08f6858034089791462
brainfck <mykhailo.tsuber@gmail.com>
2024-03-02 09:13:38 +0100

    initial commit

---
+++ SwiftUI/project2/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mykhailo Tsuber on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
