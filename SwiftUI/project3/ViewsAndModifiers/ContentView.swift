83fa9bf8b710580aa29510e7cadcd2af14dbb658
brainfck <mykhailo.tsuber@gmail.com>
2024-03-04 13:31:34 +0100

    Environment modifiers

--- SwiftUI/project3/ViewsAndModifiers/ContentView.swift
+++ SwiftUI/project3/ViewsAndModifiers/ContentView.swift
@@ -9,22 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
+    // There, font() is an environment modifier, which means the Gryffindor text view can override it with a custom font.
     VStack {
-//      Button("Hello World") {
-//        print(type(of: self.body))
-//      }
-//      .frame(width: 200, height: 200)
-//      .background(.red)
-      Text("Hello World")
-        .padding()
-        .background(.red)
-        .padding()
-        .background(.blue)
-        .padding()
-        .background(.yellow)
-        .padding()
-        .background(.green)
+      Text("Gryffindor")
+        .font(.largeTitle)
+      Text("Hufflepuff")
+      Text("Ravenclaw")
+      Text("Slytherin")
     }
+    .font(.title)
+    .padding()
+    
+    // That won’t work the same way: blur() is a regular modifier, so any blurs applied to child views are added to the VStack blur rather than replacing it.
+    VStack {
+      Text("Gryffindor")
+        .blur(radius: 0)
+      Text("Hufflepuff")
+      Text("Ravenclaw")
+      Text("Slytherin")
+    }
+    .blur(radius: 5)
   }
 }
 
