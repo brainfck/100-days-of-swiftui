cb010228cac4135f587de6538900e04bb0453fcb
brainfck <mykhailo.tsuber@gmail.com>
2024-03-05 14:28:04 +0100

    project challenge 2

---
+++ SwiftUI/project2c/RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+////  RPSApp.swift
+//  RPS
+//
+//  Created by brainfsck on 05.03.2024.
+//  
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
