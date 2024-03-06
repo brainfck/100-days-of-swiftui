e60a459d9798a88c4a69c6aae4627f7b4a688fb6
brainfck <mykhailo.tsuber@gmail.com>
2024-03-06 12:13:32 +0100

    initial commit

---
+++ SwiftUI/project4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+////  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by brainfsck on 06.03.2024.
+//  
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
